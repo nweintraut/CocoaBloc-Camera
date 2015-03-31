a50cb45c33ba4033ad4db21a87523ae1e55a0ba9
John Heaton <jheaton.dev@gmail.com>
2015-03-31 17:56:57 -0400

    correct misconfigured test target in Podfile

--- Example/Pods/Target Support Files/Pods-Tests-CocoaBloc-Camera/Pods-Tests-CocoaBloc-Camera-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Tests_CocoaBloc_Camera : NSObject
-@end
-@implementation PodsDummy_Pods_Tests_CocoaBloc_Camera
-@end
--- Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -6,36 +6,6 @@
 // project.
 
 
-// CocoaBloc-Camera
-#define COCOAPODS_POD_AVAILABLE_CocoaBloc_Camera
-#define COCOAPODS_VERSION_MAJOR_CocoaBloc_Camera 0
-#define COCOAPODS_VERSION_MINOR_CocoaBloc_Camera 1
-#define COCOAPODS_VERSION_PATCH_CocoaBloc_Camera 3
-
-// CocoaBloc-UI
-#define COCOAPODS_POD_AVAILABLE_CocoaBloc_UI
-#define COCOAPODS_VERSION_MAJOR_CocoaBloc_UI 0
-#define COCOAPODS_VERSION_MINOR_CocoaBloc_UI 0
-#define COCOAPODS_VERSION_PATCH_CocoaBloc_UI 3
-
-// CocoaBloc-UI/Colors
-#define COCOAPODS_POD_AVAILABLE_CocoaBloc_UI_Colors
-#define COCOAPODS_VERSION_MAJOR_CocoaBloc_UI_Colors 0
-#define COCOAPODS_VERSION_MINOR_CocoaBloc_UI_Colors 0
-#define COCOAPODS_VERSION_PATCH_CocoaBloc_UI_Colors 3
-
-// CocoaBloc-UI/Fonts
-#define COCOAPODS_POD_AVAILABLE_CocoaBloc_UI_Fonts
-#define COCOAPODS_VERSION_MAJOR_CocoaBloc_UI_Fonts 0
-#define COCOAPODS_VERSION_MINOR_CocoaBloc_UI_Fonts 0
-#define COCOAPODS_VERSION_PATCH_CocoaBloc_UI_Fonts 3
-
-// CocoaBloc-UI/PlaceholderTextView
-#define COCOAPODS_POD_AVAILABLE_CocoaBloc_UI_PlaceholderTextView
-#define COCOAPODS_VERSION_MAJOR_CocoaBloc_UI_PlaceholderTextView 0
-#define COCOAPODS_VERSION_MINOR_CocoaBloc_UI_PlaceholderTextView 0
-#define COCOAPODS_VERSION_PATCH_CocoaBloc_UI_PlaceholderTextView 3
-
 // Expecta
 #define COCOAPODS_POD_AVAILABLE_Expecta
 #define COCOAPODS_VERSION_MAJOR_Expecta 0
@@ -54,45 +24,9 @@
 #define COCOAPODS_VERSION_MINOR_FBSnapshotTestCase 6
 #define COCOAPODS_VERSION_PATCH_FBSnapshotTestCase 0
 
-// PureLayout
-#define COCOAPODS_POD_AVAILABLE_PureLayout
-#define COCOAPODS_VERSION_MAJOR_PureLayout 2
-#define COCOAPODS_VERSION_MINOR_PureLayout 0
-#define COCOAPODS_VERSION_PATCH_PureLayout 5
-
-// ReactiveCocoa
-#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa
-#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa 2
-#define COCOAPODS_VERSION_MINOR_ReactiveCocoa 4
-#define COCOAPODS_VERSION_PATCH_ReactiveCocoa 7
-
-// ReactiveCocoa/Core
-#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_Core
-#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_Core 2
-#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_Core 4
-#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_Core 7
-
-// ReactiveCocoa/UI
-#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_UI
-#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_UI 2
-#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_UI 4
-#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_UI 7
-
-// ReactiveCocoa/no-arc
-#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_no_arc
-#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_no_arc 2
-#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_no_arc 4
-#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_no_arc 7
-
 // Specta
 #define COCOAPODS_POD_AVAILABLE_Specta
 #define COCOAPODS_VERSION_MAJOR_Specta 0
 #define COCOAPODS_VERSION_MINOR_Specta 4
 #define COCOAPODS_VERSION_PATCH_Specta 0
-
-// pop
-#define COCOAPODS_POD_AVAILABLE_pop
-#define COCOAPODS_VERSION_MAJOR_pop 1
-#define COCOAPODS_VERSION_MINOR_pop 0
-#define COCOAPODS_VERSION_PATCH_pop 7
 
