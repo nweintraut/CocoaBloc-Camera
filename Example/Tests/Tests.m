407b5e6e9efcd27d25f62da6817fb587352a3ac6
mglagola <mark.glagola@gmail.com>
2015-04-14 14:34:05 -0500

    added image unit tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -9,3 +9,39 @@
 #import <Specta/Specta.h>
 #import <Expecta/Expecta.h>
 
+SpecBegin(TestImageResources)
+
+describe(@"Images", ^{
+    
+    NSArray *imageNames = @[@"sb_camera_arrow_down",
+                            @"sb_camera_arrow_right",
+                            @"sb_camera_checkmark",
+                            @"sb_camera_close",
+                            @"sb_camera_draw_circle",
+                            @"sb_camera_exclusive_off",
+                            @"sb_camera_exclusive_on",
+                            @"sb_camera_existing",
+                            @"sb_camera_flash_auto",
+                            @"sb_camera_flash_off",
+                            @"sb_camera_flash_on",
+                            @"sb_camera_flip",
+                            @"sb_camera_official_off",
+                            @"sb_camera_official_on",
+                            @"sb_camera_ratio_1_1",
+                            @"sb_camera_ratio_4_3",
+                            @"sb_camera_ratio_16_9",
+                            @"sb_camera_undo_circle",
+                            ];
+    
+    [imageNames enumerateObjectsUsingBlock:^(NSString *imageName, NSUInteger idx, BOOL *stop) {
+        
+        it([NSString stringWithFormat:@"should find UIImage named: %@", imageName], ^{
+            UIImage *image = [UIImage imageNamed:imageName];
+            expect(image).notTo.equal(nil);
+        });
+
+    }];
+    
+});
+
+SpecEnd
